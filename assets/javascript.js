1470f06888a550a0c4eb8a2b9fe7afb6c53140a7
admin <jon.ridewood@gmail.com>
2018-04-07 18:20:57 -0500

    javascript page scroll

--- assets/javascript.js
+++ assets/javascript.js
@@ -7,3 +7,16 @@ $(function() {
         $('projects').toggleClass('show');
     }
  });
+
+	$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
