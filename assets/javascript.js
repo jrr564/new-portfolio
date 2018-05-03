ec6b921a46d8aa88a65a7c22fbfb5f948efa71d3
admin <jon.ridewood@gmail.com>
2018-05-03 17:23:19 -0500

    animation practice

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,22 +1,17 @@
-$(function() {
-    setTimeout(function(){
-        showElement();
-     }, 30000);
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
 
-    function showElement() {
-        $('projects').toggleClass('show');
-    }
- });
+// Get the header
+var header = document.getElementById("myHeader");
 
-	$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	});
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
